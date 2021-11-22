3edfd8d22e087ff14468a52f947dda3706a15859
Roman Bogdanov <cflatminor92@gmail.com>
2021-11-22 14:07:48 +0200

    fix test

--- server.js
+++ server.js
@@ -18,9 +18,47 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(headers);
 
-app.get("/test", (req, res) => res.sendFile(
-  'view/test.html', { root: __dirname })
-);
+app.get("/test", (request, response) => {
+  const https = require('https')
+
+  const data = new TextEncoder().encode(
+    JSON.stringify({
+      todo: 'Buy the milk 🍼'
+    })
+  )
+
+  const options = {
+    hostname: 'unbounded.instructure.com',
+    port: 443,
+    path: '/api/v1/accounts',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer 18381~qAHafZFp996oNp6q57UBAdAmR9935SLUCwK14J3JlN3apfcUOIPbBm0I5PFBURcD"
+    }
+  }
+
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    var data = [];
+
+    res.on('data', d => {
+      data.push(d);
+    })
+
+    res.on('end', () => {
+      var buffer = Buffer.concat(data);
+      response.send(buffer.toString());
+    })
+  })
+
+  req.on('error', error => {
+    console.error(error);
+  })
+
+  req.write(data)
+  req.end()
+});
 
 MongoClient.connect(dbConfig.url, dbConfig.options, (error, database) => {
   if (error) return console.log(error);
